a118374870f5617b35964a935ac36d4cd4f7d780
funkysys <aadelbos@gmail.com>
2023-04-06 16:30:46 +0200

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import 'bootstrap/dist/css/bootstrap.css'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SSRProvider } from 'react-bootstrap'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SSRProvider>
       <Component {...pageProps} />
+      <Analytics />
     </SSRProvider>
   )
 }
