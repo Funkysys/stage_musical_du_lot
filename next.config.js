5e71ef41090337e006978c3f7ddea9e1673cb499
funkysys <aadelbos@gmail.com>
2023-04-06 16:24:28 +0200

    SEO Upgrade

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  siteUrl: process.env.NEXT_PUBLIC_APP_URL || 'https://lestagemusicaldulot.fr',
+  generateRobotsTxt: true,
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,18 @@ export default function Home() {
         <title>Stage Musical Du Lot</title>
         <meta name="description" content="Bienvenue dans un stage autour des musiques improvisés centré sur la bienveillance et la créativité. hâte de vous y voir !" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="all" />
+        <meta name="googlebot" content="all" />
+        <meta name="google" content="nositelinkssearchbox" />
+        <meta property="og:title" content="Stage musical du lot" />
+        <meta
+          property="og:description"
+          content="Bienvenue dans un stage autour des musiques improvisés centré sur la bienveillance et la créativité. hâte de vous y voir !"
+        />
+        <meta
+          property="og:image"
+          content="https://stagemusicaldulot.fr/assets/images/logo-asso.png"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavbarContainer />
