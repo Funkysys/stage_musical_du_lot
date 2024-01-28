d6ab9333822e982d8c5d0422863c2153a0f23bcb
Funkysys <aadelbos@gmail.com>
2024-01-28 13:59:26 +0100

    press link added

--- Components/SummerStage/PresentationContainer/PresentationContainer.jsx
+++ Components/SummerStage/PresentationContainer/PresentationContainer.jsx
@@ -8,7 +8,9 @@ gsap.registerPlugin(ScrollTrigger)
 const PresentationContainer = ({ summer }) => {
   const boxRef = useRef()
   const titleRef = useRef()
+  const titleRef2 = useRef()
   const pRef = useRef()
+  const pRef2 = useRef()
   const imageRef = useRef()
 
   const summerBG = {
@@ -42,6 +44,22 @@ const PresentationContainer = ({ summer }) => {
         trigger: boxRef.current
       }
     })
+    gsap.to(titleRef2.current, {
+      opacity: '1',
+      right: '0',
+      duration: '1.2',
+      scrollTrigger: {
+        trigger: boxRef.current
+      }
+    })
+    gsap.to(pRef2.current, {
+      opacity: '1',
+      right: '0',
+      duration: '1.2',
+      scrollTrigger: {
+        trigger: boxRef.current
+      }
+    })
     gsap.to(imageRef.current, {
       opacity: '1',
       bottom: '0',
@@ -68,6 +86,13 @@ const PresentationContainer = ({ summer }) => {
         <p>Le stage en quelques mots : </p>
         <p >{`Nous sommes très heureux de continuer cette aventure avec vous dans cette magnifique région qu’est le Lot. Le but de ce stage musical étant de partager notre passion de la musique et de donner à chacun.e l’envie de trouver sa voie à travers son parcours de musicien.ne.`}</p>
         <p >{`Ce stage est ouvert à toutes et à tous ceux qui désirent apprendre et s’exprimer à travers la musique vivante c’est à dire la pratique de son instrument, l’écoute collective et le partage avec les autres.`}</p>
+      </div>
+      <h2 ref={titleRef2} className={styles.title}>Ils en parlent :</h2>
+      <div ref={pRef2} className={styles.paragraph}>
+          <a target='_blank' href='https://www.ladepeche.fr/2023/08/22/un-tres-beau-bilan-du-stage-de-musique-improvisee-11408165.php'>Cajarc. Un très beau bilan du stage de musique improvisée</a>
+          <a target='_blank' href='https://www.ladepeche.fr/2023/06/26/un-stage-de-musique-improvisee-avec-in-extremis-11301554.php'>Cajarc. Un stage de musique improvisée avec IN Extremis</a>
+          <a target='_blank' href='https://www.tourisme-figeac.com/offres/stage-de-musique-improvisee-cajarc-fr-4263380/'>Grand Figeac Tourisme</a>
+
       </div>
       <div ref={imageRef} className={styles.image}>
         <Image
