df7c408305977fa0ba17cc80d3d51507944f40c7
funkysys <aadelbos@gmail.com>
2024-11-25 21:01:39 +0100

    Maj for winter 25

--- datas/detailsWinterDatas.js
+++ datas/detailsWinterDatas.js
@@ -1,71 +1,80 @@
 const datas = [
-    {
-        id: '1',
-        button: "RYTHME",
-        title: '10H - 11H30 : MODULE AUTOUR DU RYTHME (en groupe)',
-        content: "ce premier module sera abordé tous les matins et tous ensemble. Il permet de souder le collectif en travaillant les différents débits rythmiques au métronome. Cela permet aussi de réveiller le corps et l'esprit de manière douce."
-    },
-    {
-        id: '2',
-        button: 'CHANT ET OREILLE ',
-        title: '11H30 - 13H : MODULE D’ÉCOUTE : CHANT ET OREILLE (en demi groupe)',
-        content: "« on ne peut pas entendre ce qu’on ne peut pas chanter ». La solution ? une approche décomplexée du travail des intervalles, mouvements de basses, arpèges… en chantant en groupe dans une bonne ambiance ! Le but n'étant pas d'égaler Alicia Keys mais plutôt de donner des pistes pour être autonome et confiant.e avec sa voix et ainsi faire progresser son oreille relative."
-    },
-    {
-        id: '3',
-        button: 'COURS INDIVIDUELS',
-        title: '14H30 - 16H30 : COURS INDIVIDUELS',
-        content: "adapté à chaque élève, ce cours a pour but de mettre l’accent sur des points que l’élève souhaite aborder. C’est aussi l’occasion d’installer des routines de travail pour garantir une autonomie sur l’année. De plus, un élève guitariste pourra tout à fait être amené à recevoir un cours individuel d’un professeur qui joue d’un autre instrument. En effet, un batteur peut très bien profiter d’un cours individuel dispensé par un guitariste. C'est le principe des intelligences multiples."
-    },
-    {
-        id: '4',
-        button: 'COURS À THÈMES ',
-        title: '14H30 - 16H30 : COURS À THÈMES ',
-        content: "A lieu en parallèle des cours individuels. Ce module permet d’aborder différents sujets spécifiques (exemple : cohésion basse/batterie) qui demandent du détails à certains moments du stage pour certains instrumentistes."
-    },
-    {
-        id: '5',
-        button: 'JEUX EN GROUPE',
-        title: '16H45 - 18H45 : JEUX EN GROUPE',
-        content: "cours basé sur le répertoire qui sera joué aux concerts de fin de stage. Travail des thèmes, travail des improvisations et répartition des rôles pour chaque instrument. Un des buts de cet atelier est d’installer une atmosphère professionnelle pour appréhender du mieux possible les concerts de fin de stage."
-    },
-    {
-        id: '6',
-        button: 'JAM SESSION',
-        title: '19H - 20H : JAM SESSION',
-        content: "temps de partage où les stagiaires jouent ensemble avec un professeur. Répertoire libre. Ambiance conviviale et bienveillante."
-    },
-    {
-        id: '7',
-        button: 'SUR PLACE',
-        title: 'RÉPERTOIRES SUR PLACE',
-        content: "un morceau « défi » une composition des élèves une composition d’un professeur. La liste arrive Bientôt"
-    },
-    {
-        id: '8',
-        button: "PRÉPARÉ",
-        title: "REPERTOIRES PRÉPARÉ HIVER",
-        links: [
-            {
-                name: "All Blues - Miles Davis",
-                youtube: "https://www.youtube.com/watch?v=-488UORrfJ0&ab_channel=MilesDavisVEVO",
-                pdf: "/assets/images/all_blues.png",
-                categorie: "Spécial Kind Of Blue"
-            },
-            {
-                name: "Freddie the Freeloader - Miles Davis",
-                youtube: "https://www.youtube.com/watch?v=ZZcuSBouhVA&ab_channel=MilesDavisVEVO",
-                pdf: "/assets/images/freddie.png",
-                categorie: ""
-            },
-            {
-                name: "So What - Miles Davis",
-                youtube: "https://www.lestagemusicaldulot.fr/",
-                pdf: "/assets/images/so_what.png",
-                categorie: ""
-            }
-        ]
-    },
-]
+  {
+    id: "1",
+    button: "RYTHME",
+    title: "MODULE AUTOUR DU RYTHME (en groupe)",
+    content:
+      "ce premier module sera abordé tous les matins et tous ensemble. Il permet de souder le collectif en travaillant les différents débits rythmiques au métronome. Cela permet aussi de réveiller le corps et l'esprit de manière douce.",
+  },
+  {
+    id: "2",
+    button: "CHANT ET OREILLE ",
+    title: "MODULE D’ÉCOUTE : CHANT ET OREILLE (en demi groupe)",
+    content:
+      "« on ne peut pas entendre ce qu’on ne peut pas chanter ». La solution ? une approche décomplexée du travail des intervalles, mouvements de basses, arpèges… en chantant en groupe dans une bonne ambiance ! Le but n'étant pas d'égaler Alicia Keys mais plutôt de donner des pistes pour être autonome et confiant.e avec sa voix et ainsi faire progresser son oreille relative.",
+  },
+  {
+    id: "3",
+    button: "COURS INDIVIDUELS",
+    title: "COURS INDIVIDUELS",
+    content:
+      "adapté à chaque élève, ce cours a pour but de mettre l’accent sur des points que l’élève souhaite aborder. C’est aussi l’occasion d’installer des routines de travail pour garantir une autonomie sur l’année. De plus, un élève guitariste pourra tout à fait être amené à recevoir un cours individuel d’un professeur qui joue d’un autre instrument. En effet, un batteur peut très bien profiter d’un cours individuel dispensé par un guitariste. C'est le principe des intelligences multiples.",
+  },
+  {
+    id: "4",
+    button: "COURS À THÈMES ",
+    title: "COURS À THÈMES ",
+    content:
+      "A lieu en parallèle des cours individuels. Ce module permet d’aborder différents sujets spécifiques (exemple : cohésion basse/batterie) qui demandent du détails à certains moments du stage pour certains instrumentistes.",
+  },
+  {
+    id: "5",
+    button: "JEUX EN GROUPE",
+    title: "JEUX EN GROUPE",
+    content:
+      "cours basé sur le répertoire qui sera joué aux concerts de fin de stage. Travail des thèmes, travail des improvisations et répartition des rôles pour chaque instrument. Un des buts de cet atelier est d’installer une atmosphère professionnelle pour appréhender du mieux possible les concerts de fin de stage.",
+  },
+  {
+    id: "6",
+    button: "JAM SESSION",
+    title: "JAM SESSION",
+    content:
+      "temps de partage où les stagiaires jouent ensemble avec un professeur. Répertoire libre. Ambiance conviviale et bienveillante.",
+  },
+  {
+    id: "7",
+    button: "SUR PLACE",
+    title: "RÉPERTOIRES SUR PLACE",
+    content:
+      "Nous proposerons sur place de découvrir ou de redécouvrir des morceaux de différetents styles et époques. Ces morceaux seront travaillés en groupe et en individuel. Ils seront joués lors de la représentation de fin de stage.",
+  },
+  {
+    id: "8",
+    button: "PRÉPARÉ",
+    title: "REPERTOIRES PRÉPARÉ HIVER",
+    links: [
+      {
+        name: "Liste à venir",
+        youtube:
+          "https://www.youtube.com/watch?v=-488UORrfJ0&ab_channel=MilesDavisVEVO",
+        pdf: "/assets/images/all_blues.png",
+        categorie: "Spécial Kind Of Blue",
+      },
+      {
+        name: "Freddie the Freeloader - Miles Davis",
+        youtube:
+          "https://www.youtube.com/watch?v=ZZcuSBouhVA&ab_channel=MilesDavisVEVO",
+        pdf: "/assets/images/freddie.png",
+        categorie: "",
+      },
+      {
+        name: "So What - Miles Davis",
+        youtube: "https://www.lestagemusicaldulot.fr/",
+        pdf: "/assets/images/so_what.png",
+        categorie: "",
+      },
+    ],
+  },
+];
 
-export default datas
+export default datas;
