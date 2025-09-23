5241a23e3d052cf3f37bc3b60187b93c2a15a54e
Antoine Delbos <aadelbos@gmail.com>
2025-09-23 19:06:44 +0200

    add in extremis button link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import Link from "next/link";
 import ContactModal from "./ContactModale";
 import styles from "./Footer.module.css";
 import Legal from "./Legal";
@@ -6,6 +6,9 @@ const Footer = () => {
   return (
     <div className={styles.FooterContainer}>
       <ContactModal />
+      <Link href="https://in-extremis-formation.fr">
+        <button className={styles.inExtremisLink}>In Extremis Formation</button>
+      </Link>
       <Legal />
     </div>
   );
